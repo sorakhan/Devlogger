05b083dd9b99fec2aefb5d1608d496624dd15275
Sora Khan <thesorakhan@gmail.com>
2019-09-26 21:57:42 +1000

    first commit (section 8 started)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogsComponent,
+    LogFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
